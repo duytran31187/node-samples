99c7a4a5d20691637b0d009dea13821ef6d94df0
Duy Tran <duy.tranquoc@gforces.auto>
2023-08-02 05:24:10 +0700

    load ENV variables w dotenv package

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
 import {Addition, Substraction} from './simpleModule';
+require('dotenv').config();
 // compile module: npx tsc --module commonjs app.ts
 let addObject = new Addition(10, 20);   
 let subObject = new Substraction(20, 10);  
   
 addObject.Sum();  
-subObject.Substract();  
+subObject.Substract();
+
+console.log(`process object ${JSON.stringify(process.env)}`);
+console.log(`env name ${process.env.USER_NAME} email ${process.env.USER_EMAIL}`);
