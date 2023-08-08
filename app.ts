cb015b95ba82f5c63ee8dfd4161fbccb054f4120
Duy Tran <duy.tranquoc@gforces.auto>
2023-08-09 05:55:24 +0700

    ts-node to run ts file directly

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import {Addition, Substraction} from './simpleModule';
 require('dotenv').config();
 // compile module: npx tsc --module commonjs app.ts
+// to run file by 1 one 2 commands:
+// 1.without ts-node: npx tsc app.ts && node app.js
+// 2. with ts-node: ts-node app.ts
 let addObject = new Addition(10, 20);   
 let subObject = new Substraction(20, 10);  
   
