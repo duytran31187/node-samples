52f0c204c93f8a3c03ebdd81cad6119b98f2aca7
Duy Tran <duy.tranquoc@gforces.auto>
2024-03-18 17:54:48 +0700

    [Typescript] decorator

--- typescript-learning/tsDecoratorClass.ts
+++ typescript-learning/tsDecoratorClass.ts
@@ -1,6 +1,9 @@
+// To run: npx tsc tsDecoratorClass.ts && node tsDecoratorClass.js
+
 function WithFuel(target: typeof Rocket, context): typeof Rocket {
+  console.log(`context: ${JSON.stringify(context)}`);
     if (context.kind === "class") {
-      console.log(`init class`)  
+      console.log(`decorating class ${target.name}`);
       return class extends target {
         // fuel: number = 50
         isEmpty(): boolean {
@@ -14,14 +17,13 @@ function WithFuel(target: typeof Rocket, context): typeof Rocket {
     fuel: number = 75
   }
 
-//   @WithFuel
-//   class Car {
-//     fuel: number = 10
-//   }
+  @WithFuel
+  class Car {
+    fuel: number = 10
+  }
   
   const rocket = new Rocket()
-//   const car = new Car()
-  console.log(rocket.fuel)
-  console.log(`Is the rocket empty? ${(rocket as any).isEmpty()}`)
-//   console.log((car as any).fuel)
-  // prints 50
+  const car = new Car()
+  console.log(`is fuel empty? ${(rocket as any).isEmpty()}`);
+  // console.log(`Is the rocket empty? ${(rocket as any).isEmpty()}`)
+  // console.log(car.fuel)
