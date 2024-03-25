10810ee64e81e29f7cdb12ba7fe702f20dd69022
Duy Tran <duy.tranquoc@gforces.auto>
2024-03-25 14:01:03 +0700

    [typescript] samples for: type, enum, interface

--- typescript-learning/README.md
+++ typescript-learning/README.md
@@ -4,3 +4,11 @@ Setup: install typescript in local
 ```bash
 tsc abc.ts
 ```
+# some definitions 
+1 module: any js files without import, export or top-level awais should be considered as as script not a module. so if any script that want to be treated as a module we can add the below script
+```javascript
+export {}
+```
+ - TypeScript has ES Module syntax means: use Import && Exort
+ - CommonJS is the format which most modules on npm are delivered in => means: use module.exports && require
+ - The core primitive types in TypeScript are all lowercase! (string, number, boolean)
---
+++ typescript-learning/learning-types.ts
@@ -0,0 +1,57 @@
+type Person = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    isAdult: boolean;
+    hobbies?: string[];
+}
+
+type programmingLanguage = 'javascript' | 'typescript' | 'python' | 'java' | 'c#' | 'php';
+
+interface DeveloperRole {
+    programmingLanguage: programmingLanguage;
+    isFrontEndDeloper(): boolean;
+    isBackEndDeloper(): boolean;
+}
+enum DevRoles {
+    SENIOR = 'SENIOR',
+    JUNIOR = 'JUNIOR',
+    MIDDLE = 'MIDDLE',
+    LEADER = 'LEADER'
+}
+interface FptStaff {
+    company: string;
+    personInfo: Person;
+    Role?: DevRoles;
+}
+interface FptDeveloper extends DeveloperRole, FptStaff {
+    workingtime: number;
+}
+
+class FptDeveloperModel implements FptDeveloper {
+    personInfo: Person;
+    workingtime: number;
+    programmingLanguage: programmingLanguage;
+    company: string;
+    Role?: DevRoles;
+    
+    constructor(person: Person, workingTime: number, programmingLanguage: programmingLanguage, company: string, devRole?: DevRoles) {
+        this.personInfo = person;
+        this.workingtime = workingTime;
+        this.programmingLanguage = programmingLanguage;
+        this.company = company;
+        this.Role = devRole;
+    }
+    isAdult(): boolean {
+        return this.personInfo.isAdult;
+    }
+    isFrontEndDeloper(): boolean {
+        return ['javascript', 'typescript'].includes(this.programmingLanguage);
+    }
+    isBackEndDeloper(): boolean {
+        return ['java', 'c#', 'php'].includes(this.programmingLanguage);
+    }
+}
+
+const persion1 = new FptDeveloperModel({firstName: 'John', lastName: 'Don', age: 20, isAdult: true}, 8, 'javascript', 'FPT Software', DevRoles.JUNIOR);
+const persion2 = new FptDeveloperModel({firstName: 'evan', lastName: 'David', age: 30, isAdult: true}, 8, 'javascript', 'abc');
