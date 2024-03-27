e563f0c6ad6ffb4b0f22f2a670496cb4358b2d2e
Duy Tran <duy.tranquoc@gforces.auto>
2024-03-27 21:20:55 +0700

    [javascript] MAP

--- typescript-learning/learn-js.ts
+++ typescript-learning/learn-js.ts
@@ -13,3 +13,25 @@ letters.forEach((letter) => {
 });
 console.log(letters.has("a"));
 console.log(letters.values());
+
+//A Map holds key-value pairs where the keys can be any datatype.
+
+console.log(`-----------MAP---`);
+
+
+let students = new Map();
+students.set("John", 80);
+students.set("Jane", 95);
+students.set("Jane", 100); // overwrite
+console.log(students.size);
+console.log(`jana score: ${students.get("Jane")}`);
+
+// Create a Map
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+  
+console.log(fruits.size);
+console.log(`apples: ${fruits.get("apples")}`);  
