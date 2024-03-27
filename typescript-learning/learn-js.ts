daa61b2a59d318758640ca3298afd4953003b215
Duy Tran <duy.tranquoc@gforces.auto>
2024-03-27 21:13:42 +0700

    [javascript] Set operators

--- typescript-learning/learn-js.ts
+++ typescript-learning/learn-js.ts
@@ -2,5 +2,14 @@
 let letters = new Set();
 letters.add("a");
 letters.add("b");
+letters.add("b"); // cant add duplicate value
+letters.add("b");
 letters.add("c");
-console.log(letters);
+console.log(letters.size);
+letters.delete("b");
+
+letters.forEach((letter) => {
+    console.log(letter);
+});
+console.log(letters.has("a"));
+console.log(letters.values());
