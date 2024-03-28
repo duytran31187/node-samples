1e857a1ac35c1f5d4394e34b2f7dc2c23cb89077
Duy Tran <duy.tranquoc@gforces.auto>
2024-03-28 15:01:11 +0700

    [typescript|javascript] function apply()

--- typescript-learning/learn-js-functions.ts
+++ typescript-learning/learn-js-functions.ts
@@ -21,3 +21,14 @@ const person2 = {
 
 console.log(`person.fullName.call(person1): ${person.fullName.call(person1)}`);
 console.log(`person.address.call(person1): ${person.address.call(person1, 'Hanoi', 'Vietnam')}`);
+
+// With the apply() method, you can write a method that can be used on different objects.
+// The apply() method is similar to the call() method (previous example).
+// The difference is:
+// The call() method takes arguments separately.
+// The apply() method takes arguments as an array.
+console.log(`person.fullName.apply(person1): ${person.fullName.apply(person1)}`);
+console.log(`person.address.apply(person1): ${person.address.apply(person1, ['Hanoi', 'Vietnam'])}`);
+
+// Since JavaScript arrays do not have a max() method, you can apply the Math.max() method instead.
+console.log(`find max value of an array ${Math.max.apply(null, [1, 2, 3, 4, 5])}`); // The first argument (null) does not matter. It is not used in this example.
