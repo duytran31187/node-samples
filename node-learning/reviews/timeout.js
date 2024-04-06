0ded0a6bcb2fdcaabb08f4a6b3f9ef8cee2bbe6b
Duy Tran <duy.tranquoc@gforces.auto>
2024-04-06 08:59:56 +0700

    [node] setTimeout with delay time is 0

---
+++ node-learning/reviews/timeout.js
@@ -0,0 +1,14 @@
+// If you specify the timeout delay to 0, the callback function will be executed as soon as possible, but after the current function execution:
+setTimeout(() => {
+    console.log('timeout console log ');
+}, 0);
+  
+  console.log(' before ');
+  console.log(' before 1');
+  console.log(' before 2');
+  console.log(' before 3');
+  console.log(' before 4');
+  console.log(' before 5');
+  console.log(' before 6');
+  console.log(' before 7');
+  
