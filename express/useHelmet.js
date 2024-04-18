413667e89b29d726ac4dfab169f1bd465047bcbf
Duy Tran <duy.tranquoc@gforces.auto>
2024-04-18 09:56:41 +0700

    [express] parse request json

--- express/useHelmet.js
+++ express/useHelmet.js
@@ -9,6 +9,7 @@ app.use(express.json());                             // This is a built-in middl
 app.use(express.urlencoded({ extended: false })); // This is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads (on postman, we can see the payload type) and is based on body-parser.
 app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!, ref: https://expressjs.com/en/advanced/best-practice-security.html
 app.post('/api/user', (req, res) => {
+    // req.body is available because of express.json() middleware, which is parsed data from the request
     console.log(req.body);
     res.send(req.body);
 });
