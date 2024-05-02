50ce22cc736283d087b6ec29ffbaafecb2818d03
Duy Tran <duytran31187@gmail.com>
2024-05-02 20:55:28 +0700

    scalpe data with csv package

--- node-learning/reviews/scalpe.js
+++ node-learning/reviews/scalpe.js
@@ -1,42 +1,45 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const csvWriter = require('fast-csv').write;
+const { stringify } = require("csv-stringify");
+const { stdout } = require('process');
 
-let url = 'https://bibomart.com.vn/sua-bot-cac-loai.html?p=';
-const numPage = 2;
-let data = [];
-for (let pa = 1; pa <=numPage; pa++) {
+let url = 'https://bibomart.com.vn/be-uong-sua.html?p=';
+const numPage = 29;
+const productsContainer = '.product-item';
+const productNameContainer = '.product-item-link';
+const productPriceContainer = '.price';
+const filename = 'suabeuong.csv';
+
+const ws = fs.createWriteStream(filename);
+
+const stringifier = stringify({ header: true, columns: ['name', 'price'], quote: true });
+
+
+stringifier
+//.pipe(process.stdout)
+.pipe(ws)
+.on('finish', function() {
+      console.log('Done writing to CSV file.');
+});
+
+for (let pa = 1; pa <= numPage; pa++) {
   const link = url + pa;
   axios(link).then(response => {
     const html = response.data;
     const $ = cheerio.load(html);
-
-    // replace '.item' with the actual selector for the items you want to scrape
-    $('.product-item').each(function(i, elem) {
-      const dataItem =  {
-        name: $(this).find('.product-item-link').text().replace('\n', '').trim(), // replace '.name' with the actual selector for the name
-        price: $(this).find('.price').first().text().replace('₫', '').replaceAll(',', '').trim() // replace '.price' with the actual selector for the price
+    
+    // read data from html
+    $(productsContainer).each(function (i, elem) {
+      const dataItem = {
+        name: $(this).find(productNameContainer).text().replace('\n', '').trim(),
+        price: $(this).find(productPriceContainer).first().text().replace('₫', '').replaceAll('.', '').trim()
       };
-	    data.push(dataItem);	    
+      // console.log(`data %o`, dataItem);
+      stringifier.write(dataItem);        
     });
+    /////////////
 
-  })
-  .catch(console.error);
-}
-setTimeout(() => {
-console.log(data);
-const ws = fs.createWriteStream('output.csv', {encoding: 'utf8'});
-
-    csvWriter(data, { headers: true })
-        .pipe(ws)
-        .on('finish', function() {
-             console.log('Done writing to CSV file.');
-        });
-}, 1000);
-
-
-
-
-
+  }).catch(console.error);
+} 
 
