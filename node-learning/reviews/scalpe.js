748db26f91a6aea4946767023ac1137a2d542cd6
Duy Tran <duytran31187@gmail.com>
2024-05-02 11:59:43 +0700

    utf8 csv

--- node-learning/reviews/scalpe.js
+++ node-learning/reviews/scalpe.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const csvWriter = require('fast-csv').write;
 
 let url = 'https://bibomart.com.vn/sua-bot-cac-loai.html?p=';
-const numPage = 1;
-const data = [];
+const numPage = 2;
+let data = [];
 for (let pa = 1; pa <=numPage; pa++) {
   const link = url + pa;
   axios(link).then(response => {
@@ -14,25 +14,28 @@ for (let pa = 1; pa <=numPage; pa++) {
 
     // replace '.item' with the actual selector for the items you want to scrape
     $('.product-item').each(function(i, elem) {
-      data[i] = {
-        name: $(this).find('.product-item-link').text()	, // replace '.name' with the actual selector for the name
-        price: $(this).find('.price').text().replace('₫', '').replaceAll(',', '').trim() // replace '.price' with the actual selector for the price
+      const dataItem =  {
+        name: $(this).find('.product-item-link').text().replace('\n', '').trim(), // replace '.name' with the actual selector for the name
+        price: $(this).find('.price').first().text().replace('₫', '').replaceAll(',', '').trim() // replace '.price' with the actual selector for the price
       };
+	    data.push(dataItem);	    
     });
 
-    console.log(data);
-
-    // const ws = fs.createWriteStream('output.csv');
-
-    // csvWriter(data, { headers: true })
-    //     .pipe(ws)
-    //     .on('finish', function() {
-    //         console.log('Done writing to CSV file.');
-    //     });
   })
   .catch(console.error);
-
 }
+setTimeout(() => {
+console.log(data);
+const ws = fs.createWriteStream('output.csv', {encoding: 'utf8'});
+
+    csvWriter(data, { headers: true })
+        .pipe(ws)
+        .on('finish', function() {
+             console.log('Done writing to CSV file.');
+        });
+}, 1000);
+
+
 
 
 
