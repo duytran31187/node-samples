79a71fa262bf5c787828a80e7566827cd9602f21
Duy Tran <duytran31187@gmail.com>
2024-05-03 06:17:03 +0700

    read the links and return data in order with: async/await, promise

--- node-learning/reviews/scalpe.js
+++ node-learning/reviews/scalpe.js
@@ -13,33 +13,55 @@ const filename = 'suabeuong.csv';
 
 const ws = fs.createWriteStream(filename);
 
-const stringifier = stringify({ header: true, columns: ['name', 'price'], quote: true });
+const stringifier = stringify({ header: true, columns: ['name', 'price', 'page'], quote: true });
 
 
 stringifier
-//.pipe(process.stdout)
-.pipe(ws)
+.pipe(process.stdout)  // => print out to console
+// .pipe(ws)
 .on('finish', function() {
-      console.log('Done writing to CSV file.');
+    console.log('Done writing to CSV file.');
 });
 
-for (let pa = 1; pa <= numPage; pa++) {
-  const link = url + pa;
-  axios(link).then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    
-    // read data from html
-    $(productsContainer).each(function (i, elem) {
-      const dataItem = {
-        name: $(this).find(productNameContainer).text().replace('\n', '').trim(),
-        price: $(this).find(productPriceContainer).first().text().replace('₫', '').replaceAll('.', '').trim()
-      };
-      // console.log(`data %o`, dataItem);
-      stringifier.write(dataItem);        
-    });
-    /////////////
-
-  }).catch(console.error);
-} 
-
+const readProductsFromLink = (link) => {
+  return new Promise((resolveHandler, rejectHandler) => {
+    axios(link)
+      .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        
+        // read data from html
+        $(productsContainer).each(function (i, elem) {
+          const dataItem = {
+            name: $(this).find(productNameContainer).text().replace('\n', '').trim(),
+            price: $(this).find(productPriceContainer).first().text().replace('₫', '').replaceAll('.', '').trim(),
+            page: link
+          };
+          // console.log(`%o`, dataItem);
+          stringifier.write(dataItem);
+        });
+        resolveHandler('success');
+      })
+      .catch((e) => {
+        rejectHandler(e);
+      });
+  })
+}
+async function main() { // note: there's await function call inside so need async keyword here
+  try {
+    for(let pa = 1; pa <= numPage; pa++) {
+      const link = url + pa;
+      console.log(`%s`, link);
+      await readProductsFromLink(link) // without await, the result can be returned not in any order of pages called
+        // .then((result) => {
+        //   console.log(`done link: `, link);
+        // })
+        // .catch(e => {
+        //   console.log(`main catched err: %o`, e);
+        // })
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+main();
